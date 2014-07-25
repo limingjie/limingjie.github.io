9042c822c0a7f60226375fce46924b8f03851485
Mingjie Li <limingjie0719@gmail.com>
2014-07-26 00:15:47 +0800

    showoff

--- javascripts/showoff.js
+++ javascripts/showoff.js
@@ -1,18 +1,56 @@
-window.onload = function () {
-    if (typeof WeixinJSBridge !== "undefined") {
-        WeixinJSBridge.on('menu:share:appmessage', function (argv) {
-            WeixinJSBridge.invoke('sendAppMessage',{ 
-                "appid"     : "",
-                "img_url"   : "image/showoff.png",
-                "img_width" : "80",
-                "img_height": "80",
-                "link"      : "showoff.html",
-                "desc"      : "我与神经猫大战三天三夜, 用了一百万步围住了神经猫.",
-                "title"     : "神经猫"
-            }, function (res) {
-            });
-        });
-    } else {
-        //alert("only works in weixin");
+<script>
+    var imgUrl = "images/showoff.png";
+    var lineLink = "http://limingjie.github.io/showoff.html";
+    var descContent = "我与神经猫大战三天三夜, 终于用十万步围住了神经猫!";
+    var shareTitle = "大战神经猫";
+    var appid = '';
+     
+    function shareFriend() {
+        WeixinJSBridge.invoke('sendAppMessage',{
+            "appid": appid,
+            "img_url": imgUrl,
+            "img_width": "80",
+            "img_height": "80",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function(res) {
+            //_report('send_msg', res.err_msg);
+        })
     }
-}
+    function shareTimeline() {
+        WeixinJSBridge.invoke('shareTimeline',{
+            "img_url": imgUrl,
+            "img_width": "80",
+            "img_height": "80",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function(res) {
+               //_report('timeline', res.err_msg);
+        });
+    }
+    function shareWeibo() {
+        WeixinJSBridge.invoke('shareWeibo',{
+            "content": descContent,
+            "url": lineLink,
+        }, function(res) {
+            //_report('weibo', res.err_msg);
+        });
+    }
+    // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+        // 发送给好友
+        WeixinJSBridge.on('menu:share:appmessage', function(argv){
+            shareFriend();
+        });
+        // 分享到朋友圈
+        WeixinJSBridge.on('menu:share:timeline', function(argv){
+            shareTimeline();
+        });
+        // 分享到微博
+        WeixinJSBridge.on('menu:share:weibo', function(argv){
+            shareWeibo();
+        });
+    }, false);
+</script>
