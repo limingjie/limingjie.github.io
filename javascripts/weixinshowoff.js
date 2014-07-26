70899a51552efc37dbea42bf669e73d42daaf520
Mingjie Li <limingjie0719@gmail.com>
2014-07-26 23:38:29 +0800

    contact

--- javascripts/weixinshowoff.js
+++ javascripts/weixinshowoff.js
@@ -67,6 +67,22 @@ document.addEventListener('WeixinJSBridgeReady', function () {
     });
 }, false);
 
+function addWeixinContact() {
+    if (typeof WeixinJSBridge === 'undefined') return false;
+        WeixinJSBridge.invoke('addContact', {
+            webtype: '1',
+            username: "martin_lmj"
+        }, function (d) {
+            // 返回d.err_msg取值，d还有一个属性是err_desc
+            // add_contact:cancel 用户取消
+            // add_contact:fail　关注失败
+            // add_contact:ok 关注成功
+            // add_contact:added 已经关注
+            // WeixinJSBridge.log(d.err_msg);
+            // cb && cb(d.err_msg);
+        });
+};
+
 function getUrlParameters(parameter) {
     var url = window.location.search, arr, param, l, i;
 
